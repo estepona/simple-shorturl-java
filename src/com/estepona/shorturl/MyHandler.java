f1fd7ebcd6025854f9e004be240e3cfbddd1a2b9
estepona <esteponawondering@gmail.com>
2021-08-27 12:35:23 -0400

    restructure

--- src/com/estepona/shorturl/MyHandler.java
+++ src/com/estepona/shorturl/MyHandler.java
@@ -1,4 +1,4 @@
-package server;
+package com.estepona.shorturl;
 
 import java.io.IOException;
 import java.io.InputStream;
---
+++ src/com/estepona/shorturl/ShortUrlServer.java
@@ -0,0 +1,19 @@
+package com.estepona.shorturl;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class ShortUrlServer {
+  public static void main(String[] args) {
+    try {
+      HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8000), 0);
+      httpServer.createContext("/myapp", new MyHandler());
+      httpServer.setExecutor(null);
+      httpServer.start();
+    } catch (IOException e) {
+      e.printStackTrace(System.out);
+    }
+  }
+}
