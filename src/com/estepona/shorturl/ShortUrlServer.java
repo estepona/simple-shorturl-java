7a3d2495818888e718e28f29b8185a8a2b6b3d63
estepona <esteponawondering@gmail.com>
2021-08-27 13:27:53 -0400

    t
    req body

--- src/com/estepona/shorturl/ShortUrlServer.java
+++ src/com/estepona/shorturl/ShortUrlServer.java
@@ -3,15 +3,19 @@ package com.estepona.shorturl;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+import com.estepona.shorturl.handlers.CreateShortUrlHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class ShortUrlServer {
   public static void main(String[] args) {
     try {
       HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8000), 0);
-      httpServer.createContext("/create", new CreateShortUrlHandler());
       httpServer.setExecutor(null);
+
+      httpServer.createContext("/create", new CreateShortUrlHandler());
+
       httpServer.start();
+      System.out.println("server started");
     } catch (IOException e) {
       e.printStackTrace(System.out);
     }
--- src/com/estepona/shorturl/handlers/CreateShortUrlHandler.java
+++ src/com/estepona/shorturl/handlers/CreateShortUrlHandler.java
@@ -1,5 +1,6 @@
-package com.estepona.shorturl;
+package com.estepona.shorturl.handlers;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -10,10 +11,12 @@ import com.sun.net.httpserver.HttpHandler;
 public class CreateShortUrlHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange t) throws IOException {
-        InputStream is = t.getRequestBody();
-        System.out.println(is.toString());
+        InputStream requestBodyInputStream = t.getRequestBody();
+        ByteArrayOutputStream requestBodyTargetStream = new ByteArrayOutputStream();
+        requestBodyInputStream.transferTo(requestBodyTargetStream);
+        System.out.println("req body: " + requestBodyTargetStream.toString());
 
-        String resp = "This is the response";
+        String resp = "Create ShortUrl";
         t.sendResponseHeaders(200, resp.length());
 
         OutputStream os = t.getResponseBody();
