3565a6f9da17166d98e74897fcb6d9099cf5128b
estepona <esteponawondering@gmail.com>
2021-08-29 21:14:08 -0400

    fmt

--- app/src/main/java/com/estepona/shorturl/handler/CreateShortUrlHandler.java
+++ app/src/main/java/com/estepona/shorturl/handler/CreateShortUrlHandler.java
@@ -15,31 +15,31 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class CreateShortUrlHandler implements HttpHandler {
-    private String domain = "http://localhost:8000/";
-    private ObjectMapper om = new ObjectMapper();
+  private String domain = "http://localhost:8000/";
+  private ObjectMapper om = new ObjectMapper();
 
-    @Override
-    public void handle(HttpExchange t) throws IOException {
-        Map<String, String> params = QueryParser.parse(t.getRequestURI().getRawQuery());
-        System.out.println("req query: " + params.toString());
-        String url = params.get("url");
-        if (url == null) {
-            System.out.println("url not found...");
-            return;
-        }
-
-        InputStream requestBodyInputStream = t.getRequestBody();
-        ByteArrayOutputStream requestBodyTargetStream = new ByteArrayOutputStream();
-        requestBodyInputStream.transferTo(requestBodyTargetStream);
-        System.out.println("req body: " + requestBodyTargetStream.toString());
-
-        ShortUrlEntity entity = UrlTransformerService.transform(url);
-        CreateShortUrlResponse resp = new CreateShortUrlResponse(domain + entity.getCode());
-        String respSerialized = om.writeValueAsString(resp) + "\n";
-
-        t.sendResponseHeaders(200, respSerialized.length());
-        OutputStream os = t.getResponseBody();
-        os.write(respSerialized.getBytes());
-        os.close();
+  @Override
+  public void handle(HttpExchange t) throws IOException {
+    Map<String, String> params = QueryParser.parse(t.getRequestURI().getRawQuery());
+    System.out.println("req query: " + params.toString());
+    String url = params.get("url");
+    if (url == null) {
+      System.out.println("url not found...");
+      return;
     }
+
+    InputStream requestBodyInputStream = t.getRequestBody();
+    ByteArrayOutputStream requestBodyTargetStream = new ByteArrayOutputStream();
+    requestBodyInputStream.transferTo(requestBodyTargetStream);
+    System.out.println("req body: " + requestBodyTargetStream.toString());
+
+    ShortUrlEntity entity = UrlTransformerService.transform(url);
+    CreateShortUrlResponse resp = new CreateShortUrlResponse(domain + entity.getCode());
+    String respSerialized = om.writeValueAsString(resp) + "\n";
+
+    t.sendResponseHeaders(200, respSerialized.length());
+    OutputStream os = t.getResponseBody();
+    os.write(respSerialized.getBytes());
+    os.close();
+  }
 }
